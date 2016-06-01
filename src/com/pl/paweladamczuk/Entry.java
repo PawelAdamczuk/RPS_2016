a2009b7b9cf91c173cc75b896fe97ac74467b050
PawelAdamczuk <p.m.adamczuk@gmail.com>
2016-06-01 17:38:45 +0200

    Third commit

--- src/com/pl/paweladamczuk/Entry.java
+++ src/com/pl/paweladamczuk/Entry.java
@@ -6,6 +6,7 @@ import java.util.List;
  * Created by pawel on 6/1/16.
  */
 public class Entry {
+    public int id;
     public int timestamp;
     public int address;
     public int user;
@@ -14,12 +15,13 @@ public class Entry {
     public boolean singleEType;
     public int eType;
 
-    public Entry(int timestamp, int address, int user, List<Integer> eTypes) {
+    public Entry(int id, int timestamp, int address, int user, List<Integer> eTypes) {
         this.timestamp = timestamp;
         this.address = address;
         this.user = user;
         this.eTypes = eTypes;
         this.eTypeCount = eTypes.size();
+        this.id = id;
 
         if(eTypes.size() == 1)
         {
@@ -32,5 +34,32 @@ public class Entry {
             this.eType = 0;
         }
 
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        result.append(this.id);
+        result.append(" ");
+        result.append(this.timestamp);
+        result.append(" ");
+        result.append(this.address);
+        result.append(" ");
+        result.append(this.user);
+        result.append(" ");
+        result.append(this.eTypeCount);
+
+        return result.toString();
+    }
+
+    public String eTypesString() {
+        StringBuilder result = new StringBuilder();
+        result.append(this.id);
+        result.append(" ");
+        for (int i = 0; i < eTypeCount; i++) {
+            result.append(this.eTypes.get(i));
+            result.append(" ");
+        }
+        return result.toString();
     }
 }
--- src/com/pl/paweladamczuk/Main.java
+++ src/com/pl/paweladamczuk/Main.java
@@ -1,6 +1,7 @@
 package com.pl.paweladamczuk;
 
 import java.io.File;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -8,63 +9,123 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        int linenumber = 0;
+        String str, str2, str3;
+
+
         try {
             File stats = new File("log_phase8");
             Scanner sc = new Scanner(stats);
-
+            PrintWriter writer1 = new PrintWriter("1.txt", "UTF-8");
+            PrintWriter writer2 = new PrintWriter("2.txt", "UTF-8");
             ArrayList<Entry> data = new ArrayList<>();
 
-            while(sc.hasNextLine())
+            while(linenumber < 1128329) //1128329
             {
+
+                if (linenumber == 28329)
+                    System.out.println();
                 for (int i = 0; i < 6; i++) { //data, blah blah
-                    sc.next();
+                    str3 = sc.next();
                 }
 
-                sc.nextByte(); // (
+                str3 = sc.next();
+                str3 = str3.substring(1);
 
-                int eTypesCount = sc.nextInt();
+
+                //sc.nextByte(); // (
+
+                int eTypesCount = Integer.parseInt(str3);
+
+                if(eTypesCount > 1)
+                {
+                    System.out.println();
+                }
 
                 sc.next(); // etypes
-                sc.nextByte(); // {
+
+                //sc.skip("\\{");
+                //sc.nextByte(); // {
+
+
 
                 ArrayList<Integer> eTypes = new ArrayList<>();
 
-                for (int i = 0; i < eTypesCount; i++) {
+                str3 = sc.next();
+                str3 = str3.replaceAll("[^\\d]", "");
+
+                int temp = Integer.parseInt(str3);
+                eTypes.add(temp);
+
+                for (int i = 0; i < eTypesCount - 2; i++) {
                     eTypes.add(sc.nextInt());
                 }
 
-                sc.nextByte(); // }
-                sc.nextByte(); // )
+                if(eTypesCount > 1) {
+                    String str4 = sc.next();
+                    str4 = str4.replaceAll("[^\\d]", "");
 
-                String str = sc.next();
+                    temp = Integer.parseInt(str4);
+                    eTypes.add(temp);
+                }
+
+//                if(eTypesCount != 1)
+//                    sc.next(); // )}
+
+                //sc.skip("\\}");
+                //sc.skip("\\)");
+
+                //sc.nextByte(); // }
+                //sc.nextByte(); // )
+
+                str = sc.next();
 
                 str = str.replaceAll("[^\\d]", "");
 
                 int ip = Integer.parseInt(str);
 
-                sc.next(); // ISSUE
-                sc.next(); // authtime
+                str3 = sc.next(); // ISSUE
+                str3 = sc.next(); // authtime
+                str3 = sc.next();
+                str3 = str3.replaceAll("[^\\d]", "");
 
-                int timestamp = sc.nextInt();
+                int timestamp = Integer.parseInt(str3);
 
-                for (int i = 0; i < 5; i++) {
-                    sc.next();
+                for (int i = 0; i < 4; i++) {
+                    str3 = sc.next();
                 }
 
-                String str2 = sc.next();
+                str2 = sc.next();
 
                 str2 = str2.replaceAll("[^\\d]", "");
 
                 int usr = Integer.parseInt(str);
 
-                Entry entry = new Entry(timestamp, ip, usr, eTypes);
+                Entry entry = new Entry(linenumber, timestamp, ip, usr, eTypes);
 
-                data.add(entry);
+                writer1.println(entry);
+                writer2.println(entry.eTypesString());
+
+
+                //data.add(entry);
+
+                ++linenumber;
+
+                if(linenumber % 1000 == 0)
+                    System.gc();
             }
+            System.out.println(data.size());
+            writer1.close();
+            writer2.close();
         }
         catch (Exception e)
         {
-            System.out.println(e.getMessage());
+            for (StackTraceElement ste : Thread.currentThread().getStackTrace()) {
+                System.out.println(ste);
+            }
+            System.out.println("error: " + e.getMessage() + linenumber);
+            System.out.println(e);
         }
     }
 }
